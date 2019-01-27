5f795abc2372d6aa09e8eb16f7548388b8d71942
陈杰 <chenjie@servyou.com.cn>
2019-01-27 12:19:17 +0800

    国际化支持

--- extension.js
+++ extension.js
@@ -1,12 +1,18 @@
 var vscode = require('vscode');
 var opn = require('opn');
+var i18nHelper = require('./i18n-helper');
+
+var config = JSON.parse(process.env.VSCODE_NLS_CONFIG);
+var locale = config.locale;
+
 
 function activate(context) {
 
     var disposable = vscode.commands.registerCommand('peakchen90.openInBrowser', function (e) {
         var filename = e._fsPath;
+        var message = i18nHelper(locale, 'nonHTML.warn', 'Unable to open non-HTM or non-HTML files');
         if (!/\.html?$/i.test(filename)) {
-            vscode.window.showWarningMessage('不能打开非HTML或HTM格式的文件');
+            vscode.window.showWarningMessage(message);
             return;
         }
         opn(filename);
---
+++ i18n-helper.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var fs = require('fs');
+
+var i18nData = null;
+
+function readConfig() {
+  var i18nFolder = path.resolve(__dirname, 'i18n');
+  var configFiles = fs.readdirSync(i18nFolder).filter(function (item) {
+    if (!/\.json$/i.test(item)) {
+      return false;
+    }
+    var stat = fs.statSync(path.resolve(i18nFolder, item));
+    return stat.isFile();
+  }).map(function (item) {
+    var match = item.match(/([\w-]+)\.json/i);
+    if (match) {
+      return {
+        locale: match[1],
+        path: path.resolve(i18nFolder, item)
+      }
+    }
+  })
+
+  configFiles.forEach(function (item) {
+    try {
+      var content = fs.readFileSync(item.path);
+      i18nData[item.locale] = JSON.parse(content);
+    } catch (e) {
+      console.error(e);
+    }
+  })
+}
+
+/**
+ * 国际化i18n helper
+ * @param {string} locale 语言
+ * @param {string} key 字段
+ * @param {string} [defaultText] 默认文本，如果没找到则返回该文本
+ */
+module.exports = function (locale, key, defaultText) {
+  if (i18nData === null) {
+    i18nData = {};
+    readConfig();
+  }
+
+  var data = i18nData[locale]
+  if (!data) {
+    data = i18nData['en'] || {}
+  }
+  return data[key] || defaultText;
+}
